d2cef00e6ce8b6bf698e1497e42b11050c8218dd
AnhPhap2412 <phapdamanh@gmail.com>
2025-06-25 20:47:20 +0700

    test2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,9 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @php $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true); @endphp
+        <link rel="stylesheet" href="{{ asset('build/'.$manifest['resources/css/app.css']['file']) }}">
+        <script type="module" src="{{ asset('build/'.$manifest['resources/js/app.js']['file']) }}"></script>
         
         <!-- Custom Styling -->
         <link rel="stylesheet" href="{{ asset('css/custom-tables.css') }}">
--- resources/views/warehouses/show.blade.php
+++ resources/views/warehouses/show.blade.php
@@ -254,6 +254,7 @@
     </div>
 
     @push('scripts')
-    @vite(['resources/js/warehouse-show.js'])
+    @php $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true); @endphp
+    <script type="module" src="{{ asset('build/'.$manifest['resources/js/warehouse-show.js']['file']) }}"></script>
     @endpush
 </x-app-layout>
